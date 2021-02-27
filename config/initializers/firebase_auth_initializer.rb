ded3565f82a22d2b310f8256ee1e533e453efc2a
Diego Castillo <diegocasmo@gmail.com>
2021-02-27 07:46:27 -0500

    chore(Redis): update env variable name

--- config/initializers/firebase_auth_initializer.rb
+++ config/initializers/firebase_auth_initializer.rb
@@ -1,4 +1,4 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(url: ENV['REDIS_TLS_URL'])
+  config.redis = Redis.new(url: ENV['REDIS_URL'])
   config.project_ids = [ENV['FIREBASE_PROJECT_ID']]
 end
