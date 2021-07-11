a21e05aaf66e24613748bc568ed7817036a1f331
Diego Castillo <diegocasmo@gmail.com>
2021-07-10 19:09:07 -0500

    fix(api): rescue from parameter missing error

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,6 +3,7 @@ module ExceptionHandler
 
   included do
     rescue_from ApplicationController::Unauthorized, with: :unauthorized
+    rescue_from ActionController::ParameterMissing, with: :bad_request
     rescue_from Pundit::NotAuthorizedError, with: :unauthorized
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
@@ -15,6 +16,10 @@ module ExceptionHandler
 
     def record_not_found e
       json_response(nil, :not_found)
+    end
+
+    def bad_request e
+      json_response(e.message, :bad_request)
     end
 
     def record_invalid e
