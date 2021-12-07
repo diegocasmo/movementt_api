f1490b38c13ddc65d2f972f6055acaf60c7f7bdf
Diego Castillo <diegocasmo@gmail.com>
2021-12-07 13:41:12 -0500

    Removed logger

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,11 +6,6 @@ class Api::V1::UsersController < ApplicationController
   def me
     return json_response(current_user) if current_user.present?
 
-    logger.info("\n\n----------here----------")
-    logger.info(current_user)
-    logger.info(payload)
-    logger.info("----------here----------\n\n")
-
     raise ApplicationController::Unauthorized if payload.blank?
     user = User.find_or_create_by!(
       uid: payload[:user_id],
